70ea493ce750fd1a3d7bdd380c6b298074bbef21
isakov-m <chyraktar@gmail.com>
2025-01-29 02:54:05 +0600

    _03_Soru cozumu

---
+++ src/Gun05/_03_Soru.java
@@ -0,0 +1,41 @@
+package Gun05;
+
+import Utility.BaseDriver;
+import Utility.BaseDriverParameter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * Daha önceki derslerde yaptğımız Search fonksiyonunu
+ * mac,ipod ve samsung için Dataprovider ile yapınız.
+ */
+
+public class _03_Soru extends BaseDriver {
+
+    @Test(dataProvider = "getData")
+    void search(String aranacak){
+        WebElement search = driver.findElement(By.name("search"));
+        search.clear();
+        search.sendKeys(aranacak+ Keys.ENTER);
+
+        List<WebElement> captions = driver.findElements(By.cssSelector("[class='caption']>h4"));
+
+        for (WebElement e: captions){
+            //System.out.println("e.getText() = " + e.getText());
+            Assert.assertTrue(e.getText().toLowerCase().contains(aranacak), "Aranan kelime bulunamadi");
+        }
+    }
+
+    @DataProvider
+    Object[] getData(){
+        Object [] data = {"mac", "samsung", "ipod"};
+
+        return data;
+    }
+}
